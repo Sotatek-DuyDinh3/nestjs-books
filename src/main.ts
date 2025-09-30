d2768503d35707ee9156d1e9a98ac45e13c3f5f2
duydq2110 <quocduy37na@gmail.com>
2025-09-30 20:36:21 +0700

    feat(core): response transform and custom message decorator

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
 
+  // config transform interceptor global
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
+  // config validation pipe global
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  // config swagger
   const config = new DocumentBuilder()
-    .setTitle('Book API')
+    .setTitle('Management Book API')
     .setDescription('The Book API description')
     .setVersion('1.0')
     .build();
